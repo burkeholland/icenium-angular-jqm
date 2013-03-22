5438f088a04fd10e060d383879e99a211db5c25c
Burke Holland <burkeholland@gmail.com>
2013-03-22 13:17:36 -0500

    SoundCloud integration

--- ice-angular-jqm/js/app.js
+++ ice-angular-jqm/js/app.js
@@ -1,10 +1,27 @@
 angular.module('TodoApp', []).config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     when('/home', { templateUrl: '#home' }).
-    when('/settings', { templateUrl: '#settings' , jqmOptions: { transition: 'flip' } })
+    when('/settings/:id', { templateUrl: '#settings' , jqmOptions: { transition: 'flip' } })
 }]);
 
 function TodoCtrl($scope) {
-    $scope.things = ["thing 1", "thin 2", "thing 3"]
-  
+
+  $scope.tracks = [];
+  $scope.src = "51550786";
+  $scope.clientId = '04c79325606ff36fa1508db99f995ec8';
+
+  SC.initialize({
+    client_id: '04c79325606ff36fa1508db99f995ec8'
+  });
+
+  // find all sounds of buskers licensed under 'creative commons share alike'
+  SC.get('/tracks', { q: 'klaypex', license: 'cc-by-sa' }, function(tracks) {
+    $scope.tracks = tracks;
+    $scope.$apply();
+    console.log
+  });
+
+  // $scope.src = SC.get(track.stream_url, function(sound) {
+  //   console.log(sound);
+  // });
 }
